84d484a46b63ef27be15bf8d98ecdd4007024001
Gavin Weng <wengh7930@gmail.com>
2024-07-18 00:58:24 +1200

    update

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -3,10 +3,18 @@ import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../_utils/GlobalApi'
 import Image from 'next/image'
 import { ArrowRightCircle } from 'lucide-react'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 
 function CategoryList() {
     const listRef = useRef(null)
     const [categoryList, setCategoryList] = useState([])
+    const params = useSearchParams();
+    const [selectedCategory,setSelectedCategory] = useState('all')
+    useEffect(()=>{
+      console.log(params.get('category'));
+    },[params])
+
     useEffect(()=>{
         getCategoryList()
       },[])
@@ -32,12 +40,12 @@ function CategoryList() {
       }
   return (
     <div className='mt-10 relative'>
-      <div className='flex gap-4 overflow-auto' ref={listRef}>
+      <div className='flex gap-4 overflow-auto scrollbar-hide' ref={listRef}>
         {categoryList&&categoryList.map((category,index)=>(
-            <div key={index} className='flex flex-col items-center gap-2 border p-3 rounded-xl min-w-28 hover:border-primary hover:bg-orange-50 cursor-pointer group'>
+            <Link href={'?category='+category.slug} key={index} className={`flex flex-col items-center gap-2 border p-3 rounded-xl min-w-28 hover:border-primary hover:bg-orange-50 cursor-pointer group ${selectedCategory==category.slug&&'text-primary border-primary bg-orange-50'}`}>
                 <Image src={category.icon?.url } alt={category.name} width={50} height={50} className='group-hover:scale-125 transition-all duration-200'/>
                 <h2 className='text-sm font-medium group-hover:text-primary'>{category.name}</h2>
-            </div>
+            </Link>
         ))}
       </div>
       <ArrowRightCircle className='absolute -right-10 top-9 bg-gray-500 rounded-full text-white h-8 w-8 cursor-pointer' onClick={()=>ScrollRightHandler()}/>
