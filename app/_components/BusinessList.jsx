b9cc0366229188b5fac042fcf3f5e92f0ee23c06
Gavin Weng <wengh7930@gmail.com>
2024-07-27 21:45:13 +1200

    update

--- app/_components/BusinessList.jsx
+++ app/_components/BusinessList.jsx
@@ -4,19 +4,23 @@ import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import GlobalApi from '../_utils/GlobalApi'
 import BusinessItem from './BusinessItem';
+import BusinessItemSkelton from './BusinessItemSkelton';
 function BusinessList() {
   const params = useSearchParams();
   const [category,setCategory] = useState('all');
   const [businessList,setBusinessList] = useState([]);
+  const [loading,setLoading] = useState(false);
   useEffect (()=>{
      params&&setCategory(params.get('category'))
      params&&getBusinessList(params.get('category'))
   },[params])
 
   const getBusinessList = (category_)=>{
+    setLoading(true);
     GlobalApi.GetBusiness(category_).then(resp=>{
         console.log(resp)
         setBusinessList(resp?.restaurants);
+        setLoading(false);
     })
   }
   return (
@@ -25,9 +29,14 @@ function BusinessList() {
       <h2 className='font-bold text-primary'>{businessList?.length} Results</h2>
 
       <div className='grid grid-cols-1 sm: grid-cols-2 md: grid-cols-3 lg:grid-cols-4 gap-7 mt-3'>
-        {businessList.map((restaurants,index)=>(
+        {!loading? businessList.map((restaurants,index)=>(
           <BusinessItem key={index} business = {restaurants}/>
-        ))}
+        )):
+        
+        [1,2,3,4,5,6,7,8].map((item,index)=>(
+          <BusinessItemSkelton key={index}/>
+        ))
+        }
       </div>
     </div>
   )
