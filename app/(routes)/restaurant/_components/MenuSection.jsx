2d94d73000c6c2d0cda7aa189319804fb076bc86
Gavin Weng <wengh7930@gmail.com>
2024-07-28 22:29:28 +1200

    update

---
+++ app/(routes)/restaurant/_components/MenuSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MenuSection({restaurant}) {
+  return (
+    <div>
+      <div className='grid grid-cols-4 mt-2'>
+            <div className=''>
+                Category
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default MenuSection
---
+++ app/(routes)/restaurant/_components/RestroTabs.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import MenuSection from './MenuSection';
+
+function RestroTabs({restaurant}) {
+  return (
+    <Tabs defaultValue="category" className="w-[400px] mt-10">
+      <TabsList>
+        <TabsTrigger value="category">Category</TabsTrigger>
+        <TabsTrigger value="about">About</TabsTrigger>
+        <TabsTrigger value="reviews">Reviews</TabsTrigger>
+      </TabsList>
+      <TabsContent value="category">
+        <MenuSection restaurant={restaurant}/>
+      </TabsContent>
+      <TabsContent value="about">About</TabsContent>
+      <TabsContent value="reviews">Reviews</TabsContent>
+    </Tabs>
+  );
+}
+
+export default RestroTabs
