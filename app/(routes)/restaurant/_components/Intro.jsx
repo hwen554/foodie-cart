8bf2506d8393ce9593ebe8ea458a954868c6911f
Gavin Weng <wengh7930@gmail.com>
2024-07-29 21:17:16 +1200

    update

--- app/(routes)/restaurant/_components/Intro.jsx
+++ app/(routes)/restaurant/_components/Intro.jsx
@@ -16,6 +16,7 @@ function Intro({restaurant}) {
         </div>:
         <div className='h-[220px] w-full bg-slate-200 animate-pulse rounded-xl'>
         </div>}
+        
         <h2 className='text-3xl font-bold mt-2'>{restaurant.name}</h2>
         <div className='flex items-center gap-2 mt-2'>
             <Image src={'/star.png'} alt='star' width={20} height={20}/>
--- app/(routes)/restaurant/_components/MenuSection.jsx
+++ app/(routes)/restaurant/_components/MenuSection.jsx
@@ -1,18 +1,41 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import Image from 'next/image';
+
+
+
+import React, { useState } from 'react'
 
 function MenuSection({restaurant}) {
+
+  const[menuItemList,setMenuItemList] = useState([]);
+
+  const FilterMenu = (category)=>{
+    const result = restaurant?.menu?.filter((item)=>item.category==category)
+    setMenuItemList(result[0]);
+  }
   return (
     <div>
       <div className='grid grid-cols-4 mt-2'>
-            <div className='hidden md:flex'>
+            <div className='hidden md:flex flex-col mr-1 gap-2'>
                 {restaurant?.menu?.map((item,index)=>(
-                  <Button>{item.category}</Button>
+                  <Button variant='ghost' key={index} className='flex justify-start' onClick={()=>FilterMenu(item.category)}>{item.category}</Button>
                 ))}
             </div>
-            <div className='col-span-3'>
-                Menu List
+            <div className='col-span-3 ml-10'>
+                <h2 className='font-extrabold text-lg'>{menuItemList.category}</h2>
+
+                <div className='grid grid-cols-1 lg:grid-cols-2'>
+                  {console.log(menuItemList.menuItem)}
+                    {menuItemList?.menuItem?.map((item,index)=>(
+                      <div key={index}>
+                      <Image src={item?.productImage[0]?.url}  alt={item.price} width={120} height={120}/>
+                      <h2>{item.price}</h2>
+                      </div>
+                    ))}
+                    
+                </div>
             </div>
+
       </div>
     </div>
   )
