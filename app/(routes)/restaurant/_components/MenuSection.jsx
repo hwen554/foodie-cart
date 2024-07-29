fc0a532026594ce52b938b9a30b8764caa38ee7d
Gavin Weng <wengh7930@gmail.com>
2024-07-29 18:49:56 +1200

    Update MenuSection.jsx

--- app/(routes)/restaurant/_components/MenuSection.jsx
+++ app/(routes)/restaurant/_components/MenuSection.jsx
@@ -1,11 +1,17 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function MenuSection({restaurant}) {
   return (
     <div>
       <div className='grid grid-cols-4 mt-2'>
-            <div className=''>
-                Category
+            <div className='hidden md:flex'>
+                {restaurant?.menu?.map((item,index)=>(
+                  <Button>{item.category}</Button>
+                ))}
+            </div>
+            <div className='col-span-3'>
+                Menu List
             </div>
       </div>
     </div>
